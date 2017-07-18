ca1c0eea1171358bb565bf2f493469f7dc906d76
dyf <joicy0628@126.com>
2017-07-18 15:00:50 +0800

    add guolin weather demo init

---
+++ app/src/main/java/com/dyf/coolweather/android/MainActivity.java
@@ -0,0 +1,8 @@
+package com.dyf.coolweather.android;
+
+/**
+ * Created by dyf on 2017/7/18.
+ */
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/dyf/coolweather/android/db/Province.java
@@ -0,0 +1,41 @@
+package com.dyf.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dyf on 2017/7/18.
+ */
+
+public class Province extends DataSupport{
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
