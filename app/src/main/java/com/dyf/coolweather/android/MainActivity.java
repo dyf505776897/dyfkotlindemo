5a350102495aa8322f902508a05ac25cff7e08e3
dyf <joicy0628@126.com>
2017-07-19 16:25:47 +0800

    guolin weather demo add layout and activity

--- app/src/main/java/com/dyf/coolweather/android/MainActivity.java
+++ app/src/main/java/com/dyf/coolweather/android/MainActivity.java
@@ -1,10 +1,15 @@
 package com.dyf.coolweather.android;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 import com.dyf.dyfkotlindemo.R;
+
+import java.math.BigDecimal;
 
 /**
  * Created by dyf on 2017/7/18.
@@ -16,5 +21,12 @@ public class MainActivity extends FragmentActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        //
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/dyf/coolweather/android/util/Utility.java
+++ app/src/main/java/com/dyf/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.dyf.coolweather.android.db.City;
 import com.dyf.coolweather.android.db.County;
 import com.dyf.coolweather.android.db.Province;
+import com.dyf.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -89,6 +91,24 @@ public class Utility {
             }
         }
         return false;
+    }
+
+
+    /**
+     * 将返回的json 数据解析成Weather类
+     * @param response
+     * @return
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
